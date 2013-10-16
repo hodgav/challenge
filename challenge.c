d64fa6dc2f10f2584eb4ce1543d4889e58f6c80a
hodgav <hodgavriel@gmail.com>
2013-10-16 11:49:23 +0200

    Update challenge.c

--- challenge.c
+++ challenge.c
@@ -2,48 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include "challenge.h"
 
-void str_cpy(char** dest, char* src)
+char** str_cpy(char** dest, const char* src)
 {
     char* tempBuffer=NULL;
-    if((tempBuffer=(char*)malloc(strlen(src)+1))!=NULL)
-        strcpy(tempBuffer,src);
-    else
-        fprintf(stderr,"Error: malloc failed! (str_cpy)\n");
+    tempBuffer=(char*)malloc(strlen(src)+1);
+    strcpy(tempBuffer,src);
     free(*dest);
-    *dest=tempBuffer;	
+    *dest=tempBuffer;
+	return dest;
 }
 
 
-void str_cat(char** dest, char* src) 
+char** str_cat(char** dest, const char* src) 
 {
     int destLength,srcLength=strlen(src);
     char* tempBuffer=*dest;
     if (tempBuffer==NULL) 
     {
-      if((tempBuffer=(char*)malloc(srcLength+1))!=NULL)
-          strcpy(tempBuffer,src);
-      else
-          fprintf(stderr,"Error: malloc failed! (str_cat)\n");
+      tempBuffer=(char*)malloc(srcLength+1);
+      strcpy(tempBuffer,src);
+	  *dest=tempBuffer;
+	  return dest;
     }
-    else
-    {
       destLength=strlen(*dest);
-      if((tempBuffer=(char*)realloc(tempBuffer,destLength+srcLength+2))!=NULL)
-      {
-        strcat(tempBuffer,src);
-        strcat(tempBuffer,"!");
-      }
-      else
-        fprintf(stderr,"Error: realloc failed! (str_cat)\n");
-    }
-}
-
-int main(int argc, char *argv[])
-{
-    char *s = NULL;
-    str_cpy(&s, "Hola Hola");
-    str_cpy(&s, s+5);
-    str_cat(&s, " World");
-    puts(s); /* result should be: "Hola World!" */
+      tempBuffer=(char*)realloc(tempBuffer,destLength+srcLength+2);
+      strcat(tempBuffer,src);
+      strcat(tempBuffer,"!");
+	  *dest=tempBuffer;
+	  return dest;
 }
